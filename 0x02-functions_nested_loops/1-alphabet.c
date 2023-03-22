d3674547319662b93e83b10e8c619296b7c66721
kuttysax <kuttysax1@gmail.com>
2023-03-21 17:07:04 -0700

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * prints alphabet - prints alphabets in lowercase
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for i(i = 97; i <= 122; i++);
+	_putchar(i);
+}
+_putchar('\n');
