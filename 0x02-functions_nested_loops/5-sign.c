d6eee6d7277759edd14b3f906ce2e6c8204b974e
kuttysax <kuttysax1@gmail.com>
2023-03-22 15:00:24 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,5 @@
 /**
- * print_sign - prints the sign of a number
+ * print sign - prints the sign of a number
  * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
